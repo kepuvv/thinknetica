38b7962cf75fdeef2aba8b6e0d3ea7579f3eefa4
Kepuvv <renard.ru@gmail.com>
2017-04-18 11:59:14 +0300

    done

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ day = gets.strip.to_i
 print 'Enter month: '
 month = gets.strip.to_i
 
-print 'Enter year: '
-year = gets.strip.to_i
-
 days_in_m = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
 unless 	day >= 1 && day <= days_in_m[month - 1]
@@ -18,10 +15,31 @@ def is_year_leap? year
 	 year % 4 == 0 || year % 400 == 0
 end
 
-if is_year_leap? year
+print 'Enter year: '
+year = gets.strip.to_i
 
-	puts "Високосный" 
+if year > 1 && year < 3000
+	leap = is_year_leap? year
 else 
-	puts "Не Високосный"
+	puts 'Введите корректный год'
 end
 
+days_in_m[1] = 29 if leap
+
+day_count = 0
+x = 0
+
+while x < month - 1
+		day_count += days_in_m[x]
+		x += 1	
+end
+
+day_count += day
+
+puts "Количество дней от начала года #{day_count}"
+
+if leap
+	puts 'Год високосный'
+else
+	puts 'Год не високосный'
+end
