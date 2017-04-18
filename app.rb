2ec424db37fde985af43b2b5a93fbb39af2fdf92
Kepuvv <renard.ru@gmail.com>
2017-04-18 11:42:25 +0300

    chech date

--- app.rb
+++ app.rb
@@ -7,13 +7,19 @@ month = gets.strip.to_i
 print 'Enter year: '
 year = gets.strip.to_i
 
-days_in_m = [ 31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+days_in_m = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+unless 	day >= 1 && day <= days_in_m[month - 1]
+		puts 'Enter correct date'
+		exit
+end
 
 def is_year_leap? year
 	 year % 4 == 0 || year % 400 == 0
 end
 
 if is_year_leap? year
+
 	puts "Високосный" 
 else 
 	puts "Не Високосный"
