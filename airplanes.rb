407f48207d3fe0e2f6c291b5447b5af78cde2ba8
Kepuvv <renard.ru@gmail.com>
2017-04-26 17:23:41 +0300

    add class railwaystation

--- airplanes.rb
+++ airplanes.rb
@@ -1,2 +1,26 @@
-# 3 урок, Classes, def диспетчер аэропорта
+# 3 урок, Classes, def, начало диспетчер ЖД
 
+class Train
+
+end
+
+class RailwayStation
+
+	attr_reader :trains
+
+	def initialize (name)
+		@name = name
+		@trains = []
+	end
+
+	def arrive_train (train)
+		@trains << train
+	end
+
+	def send_train (train)
+		@trains.delete(train)
+end
+
+class Route
+
+end
