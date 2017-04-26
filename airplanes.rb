9f07ef19fd0e619e0dbe61a1015abd693b81e5a0
Kepuvv <renard.ru@gmail.com>
2017-04-26 17:47:51 +0300

    add route

--- airplanes.rb
+++ airplanes.rb
@@ -8,19 +8,33 @@ class RailwayStation
 
 	attr_reader :trains
 
-	def initialize (name)
+	def initialize(name)
 		@name = name
 		@trains = []
 	end
 
-	def arrive_train (train)
+	def arrive_train(train)
 		@trains << train
 	end
 
-	def send_train (train)
+	def send_train(train)
 		@trains.delete(train)
 end
 
 class Route
 
+	attr_reader :stations
+
+	# над как-то придумать,чтобы станция оставалась последней
+	def initialize(start_station, end_station)
+		@start_station = start_station
+		@end_station = end_station
+		@stations = []
+		@stations[0] = @start_station
+		@stations[-1] = @end_station
+	end
+
+	def add_station(station, position)
+		@stations[position] = station
+	end
 end
